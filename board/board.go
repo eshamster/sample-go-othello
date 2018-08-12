7cf3831413fbdb0d808cc95dccbeb13a407b0c9b
eshamster <hamgoostar@gmail.com>
2018-08-12 04:10:33 +0000

    Add board

---
+++ board/board.go
@@ -0,0 +1,215 @@
+package board
+
+import (
+	"../move"
+	"fmt"
+	"io"
+	"log"
+)
+
+// Board is a structure having board information
+type Board struct {
+	black, white uint64
+}
+
+const (
+	// Empty square state
+	Empty = iota
+	// White square state
+	White
+	// Black square state
+	Black
+)
+
+// Direction
+const (
+	top = iota
+	topRight
+	right
+	bottomRight
+	bottom
+	bottomLeft
+	left
+	topLeft
+)
+
+func (board *Board) getSquareState(move move.Move) int {
+	moveBit := move.GetBit()
+
+	if board.black&moveBit != 0 {
+		return Black
+	} else if board.white&moveBit != 0 {
+		return White
+	} else {
+		return Empty
+	}
+}
+
+func (board *Board) setMoveRaw(move move.Move, isWhite bool) {
+	moveBit := move.GetBit()
+
+	if isWhite {
+		board.white |= moveBit
+		board.black &= ^moveBit
+	} else {
+		board.black |= moveBit
+		board.white &= ^moveBit
+	}
+}
+
+// MakeBoard makes a initialized board
+func MakeBoard() Board {
+	board := Board{}
+
+	board.setMoveRaw(move.MakeMove(3, 4), true)
+	board.setMoveRaw(move.MakeMove(4, 3), true)
+	board.setMoveRaw(move.MakeMove(3, 3), false)
+	board.setMoveRaw(move.MakeMove(4, 4), false)
+
+	return board
+}
+
+func getNextMoveBit(moveBit uint64, direction int) (uint64, bool) {
+	maskToBottom := uint64(0x00ffffffffffffff)
+	maskToTop := uint64(0xffffffffffffff00)
+	maskToRight := uint64(0x7f7f7f7f7f7f7f7f)
+	maskToLeft := uint64(0xfefefefefefefefe)
+
+	result := uint64(0)
+
+	switch direction {
+	case top:
+		result = (moveBit << 8) & maskToTop
+	case topRight:
+		result = (moveBit << 7) & maskToTop & maskToRight
+	case right:
+		result = (moveBit >> 1) & maskToRight
+	case bottomRight:
+		result = (moveBit >> 9) & maskToBottom & maskToRight
+	case bottom:
+		result = (moveBit >> 8) & maskToBottom
+	case bottomLeft:
+		result = (moveBit >> 7) & maskToBottom & maskToLeft
+	case left:
+		result = (moveBit << 1) & maskToLeft
+	case topLeft:
+		result = (moveBit << 9) & maskToTop & maskToLeft
+	default:
+		log.Fatalf("Unrecognized direction: %d", direction)
+	}
+
+	return result, result != 0
+}
+
+// Find pieces that can be reversed from move point towards the direction.
+// Return 0 if no pieces can be reversed.
+func (board *Board) findReversePieces(moveBit uint64, isWhite bool, direction int) uint64 {
+	myBoard := board.white
+	otherBoard := board.black
+
+	if !isWhite {
+		myBoard = board.black
+		otherBoard = board.white
+	}
+
+	result := uint64(0)
+	findsMine := false
+
+	targetBit := moveBit
+	getsNext := false
+
+	for {
+		targetBit, getsNext = getNextMoveBit(targetBit, direction)
+		if !getsNext {
+			break
+		}
+
+		if myBoard&targetBit != 0 {
+			findsMine = true
+			break
+		} else if otherBoard&targetBit != 0 {
+			result |= targetBit
+		} else { /* empty case */
+			break
+		}
+	}
+
+	if !findsMine || (result == 0) {
+		return 0
+	}
+
+	return result
+}
+
+// SetMove sets move to board with reversing opponent pieces.
+// It returns true if the move is legal, otherwise returns false.
+func (board *Board) SetMove(move move.Move, isWhite bool) bool {
+	moveBit := move.GetBit()
+
+	if board.white&moveBit != 0 ||
+		board.black&moveBit != 0 {
+		// Not empty case
+		return false
+	}
+
+	result := false
+
+	for i := 0; i < 8; i++ {
+		reversePieces := board.findReversePieces(moveBit, isWhite, i)
+
+		if reversePieces != 0 {
+			if isWhite {
+				board.white |= reversePieces
+				board.black &= ^reversePieces
+			} else {
+				board.black |= reversePieces
+				board.white &= ^reversePieces
+			}
+			result = true
+		}
+	}
+
+	if result {
+		board.setMoveRaw(move, isWhite)
+	}
+
+	return result
+}
+
+// IsLegalMove returns the move is legal or not
+func (board *Board) IsLegalMove(move move.Move, isWhite bool) bool {
+	if board.getSquareState(move) != Empty {
+		return false
+	}
+
+	moveBit := move.GetBit()
+
+	for i := 0; i < 8; i++ {
+		if board.findReversePieces(moveBit, isWhite, i) != 0 {
+			return true
+		}
+	}
+
+	return false
+}
+
+func printSquare(w io.Writer, state int) {
+	switch state {
+	case Empty:
+		fmt.Fprint(w, "-")
+	case White:
+		fmt.Fprint(w, "o")
+	case Black:
+		fmt.Fprint(w, "x")
+	}
+}
+
+// PrintBoard prints board to standard output
+func (board *Board) PrintBoard(w io.Writer) {
+	for y := uint(0); y < 8; y++ {
+		for x := uint(0); x < 8; x++ {
+			printSquare(w, board.getSquareState(move.MakeMove(x, y)))
+		}
+		fmt.Fprint(w, "\n")
+	}
+}
---
+++ board/board_test.go
@@ -0,0 +1,143 @@
+package board
+
+import (
+	"../move"
+	"bytes"
+	"testing"
+)
+
+func getBoardString(board *Board) string {
+	buf := &bytes.Buffer{}
+	board.PrintBoard(buf)
+	return buf.String()
+}
+
+func checkBoardResult(t *testing.T, expectedString string, board *Board) {
+	result := getBoardString(board)
+
+	if expectedString != result {
+		t.Errorf("Expected:\n%s\nResult:\n%s", expectedString, result)
+	}
+}
+
+func TestBoardPrint(t *testing.T) {
+	board := MakeBoard()
+	expected := `--------
+--------
+--------
+---xo---
+---ox---
+--------
+--------
+--------
+`
+	checkBoardResult(t, expected, &board)
+}
+
+func setPiecesRaw(board *Board, pieces [][2]uint, isWhite bool) {
+	for _, piece := range pieces {
+		x := piece[0]
+		y := piece[1]
+
+		board.setMoveRaw(move.MakeMove(x, y), isWhite)
+	}
+}
+
+func TestSetMove(t *testing.T) {
+	var board Board
+	var expected string
+	var retval bool
+
+	// Reverse multiple piece and multiple direction
+	board = MakeBoard()
+	setPiecesRaw(&board, [][2]uint{{4, 6}}, true)
+	setPiecesRaw(&board, [][2]uint{{4, 4}, {5, 4}, {4, 5}, {5, 5}, {3, 7}}, false)
+	expected = `--------
+--------
+--------
+---xo---
+---oxx--
+----xx--
+----o---
+---x----
+`
+	checkBoardResult(t, expected, &board)
+
+	retval = board.SetMove(move.MakeMove(6, 4), true)
+
+	if !retval {
+		t.Errorf("Failed to set move to (6,4)")
+	}
+
+	expected = `--------
+--------
+--------
+---xo---
+---oooo-
+----xo--
+----o---
+---x----
+`
+	checkBoardResult(t, expected, &board)
+
+	// Stop by empty or wall
+	board = MakeBoard()
+	setPiecesRaw(&board, [][2]uint{{4, 6}}, true)
+	setPiecesRaw(&board, [][2]uint{{3, 4}, {5, 4}, {7, 4}, {4, 5}, {5, 5}}, false)
+	expected = `--------
+--------
+--------
+---xo---
+---xxx-x
+----xx--
+----o---
+--------
+`
+	checkBoardResult(t, expected, &board)
+
+	retval = board.SetMove(move.MakeMove(6, 4), true)
+
+	if !retval {
+		t.Errorf("Failed to set move to (6,4)")
+	}
+
+	expected = `--------
+--------
+--------
+---xo---
+---xxxox
+----xo--
+----o---
+--------
+`
+	checkBoardResult(t, expected, &board)
+}
+
+func testIsLegal(t *testing.T, x, y uint, isWhite, expected bool) {
+	board := MakeBoard()
+	result := board.IsLegalMove(move.MakeMove(x, y), isWhite)
+
+	if expected != result {
+		if expected {
+			t.Errorf("Move(%d,%d) should be legal", x, y)
+		} else {
+			t.Errorf("Move(%d,%d) should be illegal", x, y)
+		}
+	}
+}
+
+func TestIsLegalMove(t *testing.T) {
+	// legal case
+	testIsLegal(t, 2, 3, true, true)
+	testIsLegal(t, 3, 5, false, true)
+	// illegal: next to my piece
+	testIsLegal(t, 3, 5, true, false)
+	testIsLegal(t, 2, 3, false, false)
+	// illegal: isolated case
+	testIsLegal(t, 1, 1, true, false)
+	testIsLegal(t, 1, 1, false, false)
+	// illegal: on exsiting piece
+	testIsLegal(t, 3, 3, true, false)
+	testIsLegal(t, 3, 3, false, false)
+	// TODO: illegal: out of board
+}
